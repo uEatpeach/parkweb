dd1d183e8f21cfecaba773034155152c82a2c96a
uEatpeach <2216109163@qq.com>
2023-12-04 17:58:06 +0800

    FastJson车牌号数据bug修改

--- src/main/java/com/wkj/utils/FastJson.java
+++ src/main/java/com/wkj/utils/FastJson.java
@@ -3,10 +3,18 @@ package com.wkj.utils;
 import com.alibaba.fastjson.JSONObject;
 
 public class FastJson {
+
     public static String getJsonString(String str) {
         JSONObject jsonObject = JSONObject.parseObject(str);
         String r = jsonObject.getString("words_result");
         JSONObject jsonObject1 = JSONObject.parseObject(r);
         return jsonObject1.getString("number");
+
+
+        /*JSONObject jsonObject = JSONObject.parseObject(str);
+        JSONArray jsonArray = jsonObject.getJSONArray("words_result");
+        JSONObject obj = jsonArray.getJSONObject(0);
+        String words =obj.getString("words");
+        return words;*/
     }
 }
